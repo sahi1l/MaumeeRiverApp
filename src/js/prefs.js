f835e576793821726263e5bd5a3ce2bccbba0fe7
Sam Hill <physhill@gmail.com>
2024-12-31 17:51:43 -0500

    Fixed preferences by wrapping the value in a String function

--- src/js/prefs.js
+++ src/js/prefs.js
@@ -11,30 +11,16 @@ export let Prefs = new class {
     async load() {
         let num;
         let text="";
-        if(Preferences) {
-            num = await Preferences.get({key:this.name});
-            text += "-P";
-            document.querySelector("#canvas").style.backgroundColor = "#040";
-        } else {
-            document.querySelector("#canvas").style.backgroundColor = "#044";
-            num=Cookies.get(this.name);
-            text += "-C";
-        }
+        num = await Preferences.get({key:this.name});
+        text += "-P";
         if(num===undefined) {num=1;} else {num=Number(num.value);}
-        $("#prefDEBUG").html(text+num);
         this.left = Boolean(num&4);
         this.auto = Boolean(num&2);
         this.rules = Boolean(num&1);
     }
     async save() {
         let code = (this.left<<2) + (this.auto<<1) + (this.rules<<0);
-        if(Preferences) {
-            await Preferences.set({key:this.name, value:code});
-            $("#prefDEBUG").html("*P"+code);
-        } else {
-            Cookies.set(this.name,code);
-            $("#prefDEBUG").html("*C"+code);
-        }
+        await Preferences.set({key:this.name, value:String(code)});
     }
     toggle(which) {
         this[which] = !this[which];
